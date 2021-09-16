95dcecc05720a45c3a9e560177fefded98903e9e
Maks Stachowiak <maks.stachowiak@swmansion.com>
2021-09-16 13:28:43 +0200

    finished up the bin

--- examples/hls_sink.exs
+++ examples/hls_sink.exs
@@ -6,37 +6,126 @@ Mix.install([
   :membrane_hackney_plugin
 ])
 
-defmodule MyBin do
+defmodule MyBin do # TODO Membrane.HTTPAdaptiveStream.SomeNameBin jak go nazwać? jak opisać?
   use Membrane.Bin
 
-  def_input_pad :input, demand_unit: :buffers, caps: :any
-  # def_output_pad :output, demand_unit: :buffers, caps: :any
-  def_options segment_duration: [type: :integer]
+  def_options muxer_segment_duration: [
+                type: :time,
+                default: 2 |> Membrane.Time.seconds()
+              ],
+              manifest_name: [
+                type: :string,
+                spec: String.t(),
+                default: "index",
+                description: "Name of the main manifest file"
+              ],
+              manifest_module: [
+                type: :atom,
+                spec: module,
+                description: """
+                Implementation of the `Membrane.HTTPAdaptiveStream.Manifest`
+                behaviour.
+                """
+              ],
+              storage: [
+                type: :struct,
+                spec: Storage.config_t(),
+                description: """
+                Storage configuration. May be one of `Membrane.HTTPAdaptiveStream.Storages.*`.
+                See `Membrane.HTTPAdaptiveStream.Storage` behaviour.
+                """
+              ],
+              target_window_duration: [
+                spec: pos_integer | :infinity,
+                type: :time,
+                default: Membrane.Time.seconds(40),
+                description: """
+                Manifest duration is keept above that time, while the oldest segments
+                are removed whenever possible.
+                """
+              ],
+              persist?: [
+                type: :bool,
+                default: false,
+                description: """
+                If true, stale segments are removed from the manifest only. Once
+                playback finishes, they are put back into the manifest.
+                """
+              ],
+              target_segment_duration: [
+                type: :time,
+                default: 0,
+                description: """
+                Expected length of each segment. Setting it is not necessary, but
+                may help players achieve better UX.
+                """
+              ]
+
+  def_input_pad :input,
+  demand_unit: :buffers,
+  caps: [Membrane.H264, Membrane.AAC],
+  availability: :on_request,
+  options: [
+    encoding: [
+      spec: :H264 | :AAC,
+      description: """
+      Encoding atom determining which payloader will be used.
+      """
+    ]
+  ]
 
   @impl true
   def handle_init(opts) do
     children = [
-      payloader: Membrane.MP4.Payloader.H264,
-      cmaf_muxer: %Membrane.MP4.CMAF.Muxer{
-        segment_duration: opts.segment_duration |> Membrane.Time.seconds()
-      },
       sink: %Membrane.HTTPAdaptiveStream.Sink{
-        manifest_module: Membrane.HTTPAdaptiveStream.HLS,
-        target_window_duration: 30 |> Membrane.Time.seconds(),
-        target_segment_duration: 2 |> Membrane.Time.seconds(),
-        persist?: false,
-        storage: %Membrane.HTTPAdaptiveStream.Storages.FileStorage{directory: __DIR__}
+        manifest_name: opts.manifest_name,
+        manifest_module: opts.manifest_module,
+        storage: opts.storage,
+        target_window_duration: opts.target_window_duration,
+        persist?: opts.persist?,
+        target_segment_duration: opts.target_segment_duration,
       }
     ]
 
+    {{:ok, spec: %ParentSpec{children: children, links: []}}, %{muxer_segment_duration: opts.muxer_segment_duration}}
+  end
+
+  @impl true
+  def handle_pad_added(Pad.ref(:input, ref) = pad, context, state) do
+    muxer_module = %Membrane.MP4.CMAF.Muxer{segment_duration: state.muxer_segment_duration}
+    payloader_module = case context.options[:encoding] do
+      :H264 ->
+        Membrane.MP4.Payloader.H264
+      :AAC ->
+        Membrane.MP4.Payloader.AAC
+    end
+
     links = [
-      link_bin_input()
-      |> to(:payloader)
-      |> to(:cmaf_muxer)
+      link_bin_input(pad)
+      |> to({:payloader, ref}, payloader_module)
+      |> to({:cmaf_muxer, ref}, muxer_module)
       |> to(:sink)
     ]
 
-    {{:ok, spec: %ParentSpec{children: children, links: links}}, %{}}
+    children = [
+      {:payloader, ref} => payloader_module,
+      {:cmaf_muxer, ref} => muxer_module
+    ]
+
+    {{:ok, spec: %ParentSpec{children: children, links: links}}, %{}} #! tu czyszczę state'a
+  end
+
+  @impl true
+  def handle_pad_removed(Pad.ref(:input, ref), _ctx, state) do
+    children =
+              [
+
+
+
+        {:payloader, ref},
+        {:cmaf_muxer, ref}
+      ]
+    {{:ok, remove_child: children}, state}
   end
 
   @impl true
@@ -93,22 +182,28 @@ defmodule Example do
         alignment: :au,
         attach_nalus?: true
       },
-      bin: %MyBin{
-        segment_duration: 2
+      my_bin: %MyBin{
+        muxer_segment_duration: 2 |> Membrane.Time.seconds(),
+        manifest_module: Membrane.HTTPAdaptiveStream.HLS,
+        target_window_duration: 30 |> Membrane.Time.seconds(),
+        target_segment_duration: 2 |> Membrane.Time.seconds(),
+        persist?: false,
+        storage: %Membrane.HTTPAdaptiveStream.Storages.FileStorage{directory: __DIR__}
       }
     ]
 
     links = [
       link(:source)
       |> to(:parser)
-      |> to(:bin)
+      |> via_in(:input, options: [encoding: :H264])
+      |> to(:my_bin)
     ]
 
     {{:ok, spec: %ParentSpec{children: children, links: links}}, %{}}
   end
 
   @impl true
-  def handle_notification(:end_of_stream, :bin, _context, state) do
+  def handle_notification(:end_of_stream, :my_bin, _context, state) do
     Membrane.Pipeline.stop_and_terminate(self())
     {:ok, state}
   end
