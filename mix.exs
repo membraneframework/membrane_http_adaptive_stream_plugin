820be6a30ca68c2f2ae6ec5e7c70d0d043467d72
Daniel Jodłoś <daniel.jodlos@swmansion.com>
2021-09-22 10:31:41 +0200

    Fix mix.exs

--- mix.exs
+++ mix.exs
@@ -62,7 +62,7 @@ defmodule Membrane.HTTPAdaptiveStream.MixProject do
       {:membrane_cmaf_format, "~> 0.3.0"},
       {:credo, "~> 1.5.6"},
       {:ex_doc, "~> 0.25", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false}
+      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
       {:membrane_mp4_plugin, "~> 0.7.0"}
     ]
   end
