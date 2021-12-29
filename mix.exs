38e705e18f517e7389f4e3fb631cb91eed2d4e6d
Michał Śledź <sledzson08@gmail.com>
2021-12-29 17:00:37 +0100

    Update deps. Bump version to 0.5.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.HTTPAdaptiveStream.MixProject do
   use Mix.Project
 
-  @version "0.4.1"
+  @version "0.5.0"
   @github_url "https://github.com/membraneframework/membrane_http_adaptive_stream_plugin"
 
   def project do
@@ -62,13 +62,13 @@ defmodule Membrane.HTTPAdaptiveStream.MixProject do
     [
       {:membrane_core, "~> 0.8.0"},
       {:membrane_cmaf_format, "~> 0.4.0"},
+      {:membrane_mp4_plugin, "~> 0.10.0"},
       {:credo, "~> 1.5.6"},
       {:ex_doc, "~> 0.25", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
-      {:membrane_mp4_plugin, "~> 0.9.0"},
       {:membrane_hackney_plugin, "~> 0.6.0", only: [:test]},
-      {:membrane_h264_ffmpeg_plugin, "~> 0.14.0", only: [:test]},
-      {:membrane_aac_plugin, "~> 0.9.0", only: [:test]}
+      {:membrane_h264_ffmpeg_plugin, "~> 0.16.0", only: [:test]},
+      {:membrane_aac_plugin, "~> 0.11.0", only: [:test]}
     ]
   end
 end
