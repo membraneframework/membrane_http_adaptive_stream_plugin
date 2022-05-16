77f11c6b3cf4bb3857a5448fda917be4d6b318c8
Michał Śledź <michalsledz34@gmail.com>
2022-05-16 14:05:12 +0200

    Update deps

--- mix.exs
+++ mix.exs
@@ -66,16 +66,13 @@ defmodule Membrane.HTTPAdaptiveStream.MixProject do
       {:membrane_core, "~> 0.10.0"},
       {:membrane_cmaf_format, "~> 0.6.0"},
       {:membrane_tee_plugin, "~> 0.9.0"},
-      {:credo, "~> 1.6.1", only: [:dev, :test], runtime: false},
+      {:membrane_mp4_plugin, "~> 0.14.0"},
+      {:credo, "~> 1.6.1", only: :dev, runtime: false},
       {:ex_doc, "~> 0.25", only: :dev, runtime: false},
       {:dialyxir, "~> 1.1", only: :dev, runtime: false},
-      # {:membrane_mp4_plugin, "~> 0.13.0"},
-      {:membrane_mp4_plugin,
-       github: "membraneframework/membrane_mp4_plugin", branch: "update-deps"},
-      {:membrane_hackney_plugin, "~> 0.8.0", only: [:test]},
-      {:membrane_h264_ffmpeg_plugin, "~> 0.20.0", only: [:test]},
-      # {:membrane_h264_ffmpeg_plugin, path: "../membrane_h264_ffmpeg_plugin", only: [:test]},
-      {:membrane_aac_plugin, "~> 0.12.0", only: [:test]}
+      {:membrane_hackney_plugin, "~> 0.8.0", only: :test},
+      {:membrane_h264_ffmpeg_plugin, "~> 0.20.0", only: :test},
+      {:membrane_aac_plugin, "~> 0.12.0", only: :test}
     ]
   end
 
