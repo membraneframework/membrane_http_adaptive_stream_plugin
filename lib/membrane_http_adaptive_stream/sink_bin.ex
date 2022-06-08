8bbdccf1af4f9c87f454904a529b5b291cb51c29
Jakub Perżyło <qizot1405@gmail.com>
2022-06-08 10:00:11 +0200

    Add start of stream notification from SinkBin

--- lib/membrane_http_adaptive_stream/sink_bin.ex
+++ lib/membrane_http_adaptive_stream/sink_bin.ex
@@ -186,6 +186,11 @@ defmodule Membrane.HTTPAdaptiveStream.SinkBin do
   end
 
   @impl true
+  def handle_element_start_of_stream({:sink, _}, _ctx, state) do
+    {{:ok, notify: :start_of_stream}, state}
+  end
+
+  @impl true
   def handle_element_end_of_stream({:sink, _}, _ctx, state) do
     {{:ok, notify: :end_of_stream}, state}
   end
