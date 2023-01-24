89e036e8b041d72c7485bc2650c7482e2b04fd0f
Przemysław Rożnawski <roznawski.przemyslaw@gmail.com>
2023-01-24 15:53:09 +0100

    Simplified bandwidth calculation.

--- lib/membrane_http_adaptive_stream/bandwidth_calculator.ex
+++ lib/membrane_http_adaptive_stream/bandwidth_calculator.ex
@@ -1,6 +1,8 @@
 defmodule Membrane.HTTPAdaptiveStream.BandwidthCalculator do
   @moduledoc """
   Functions to calculate multimedia track bandwidth according to: https://datatracker.ietf.org/doc/html/rfc8216#section-4.3.4.2
+
+  For a single track it comes down to finding a single segment with the highest bitrate, equal to byte_size to duration ratio.
   """
 
   use Ratio, comparison: true
@@ -14,58 +16,20 @@ defmodule Membrane.HTTPAdaptiveStream.BandwidthCalculator do
   @default_bandwidth 2_560_000
 
   @spec calculate_bandwidth(Track.t(), integer()) :: integer()
-  def calculate_bandwidth(track, packets_number \\ 10) do
-    target_duration = track.target_segment_duration / Time.second()
+  def calculate_bandwidth(track, segments_number \\ 20) do
+    segments =
+      track.segments
+      |> Enum.to_list()
+      |> Enum.take(-segments_number)
+      |> Enum.filter(&(&1.duration > 0))
 
-    segments_sequences =
-      generate_subsequences(
-        track.segments
-        |> Enum.to_list()
-        |> Enum.take(-packets_number)
-      )
-
-    segments_meta =
-      segments_sequences
-      |> Enum.map(
-        &{
-          Enum.map(&1, fn sg -> 8 * sg.byte_size end) |> Enum.sum(),
-          Enum.map(&1, fn sg -> sg.duration / Time.second() end)
-          |> Enum.reduce(fn acc, x -> acc + x end)
-        }
-      )
-      |> Enum.filter(fn {_bits_size, duration} -> duration != 0.0 end)
-
-    segments_bitrates =
-      segments_meta |> Enum.map(fn {bits_size, duration} -> bits_size / duration end)
-
-    # According to HLS rfc only segment subsequences with duration between 0.5 and 1.5 of target duration should be
-    # considered
-    validated_segments_meta =
-      segments_meta
-      |> Enum.filter(fn {_bits_size, duration} ->
-        duration >= 0.5 * target_duration and duration <= 1.5 * target_duration
-      end)
-
-    validated_segments_bitrates =
-      validated_segments_meta |> Enum.map(fn {bits_size, duration} -> bits_size / duration end)
-
-    cond do
-      Enum.empty?(segments_meta) ->
-        @default_bandwidth
-
-      Enum.empty?(validated_segments_meta) ->
-        segments_bitrates |> Enum.max(&Ratio.>=/2) |> Ratio.trunc()
-
-      true ->
-        validated_segments_bitrates |> Enum.max(&Ratio.>=/2) |> Ratio.trunc()
+    if Enum.empty?(segments) do
+      @default_bandwidth
+    else
+      segments
+      |> Enum.map(fn sg -> 8 * sg.byte_size / (sg.duration / Time.second()) end)
+      |> Enum.max(&Ratio.>=/2)
+      |> Ratio.trunc()
     end
-  end
-
-  # Generates all contiguous subsequences of a list. This is needed because rfc defines bandwidth
-  # in HLS as the highest size to duration ratio of all contiguous subsequences of media track segments.
-  defp generate_subsequences(sequence) do
-    n = length(sequence)
-    subsequence_ranges = for(i <- 0..(n - 1), do: for(j <- 0..i, do: {j, i})) |> List.flatten()
-    Enum.map(subsequence_ranges, fn {i, j} -> Enum.slice(sequence, i..j) end)
   end
 end
