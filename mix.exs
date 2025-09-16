3e05e2a75b62bc5ffc89df6499d57876d2e2e024
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2025-09-16 14:19:27 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.HTTPAdaptiveStream.MixProject do
   use Mix.Project
 
-  @version "0.18.8"
+  @version "0.20.0"
   @github_url "https://github.com/membraneframework/membrane_http_adaptive_stream_plugin"
 
   def project do
